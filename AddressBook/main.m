62ac82120cf639faff763f197eb24fb914936e72
Princess Sampson <ps@kingonomics.com>
2016-08-22 15:20:40 -0400

    Initial Commit

---
+++ AddressBook/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AddressBook
+//
+//  Created by Princess Sampson on 8/22/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
